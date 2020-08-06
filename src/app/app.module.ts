a384760216d86f3170cdbf16007795e8d0fee6a7
Wilmir Nicanor <wbnicanor01@gmail.com>
2020-08-06 19:54:31 +0100

    Creates a separate component for the menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { DateAgoPipe } from './pipes/date-ago.pipe';
 import { D3legendService } from './services/d3legend.service';
 import { D3tooltipService } from './services/d3tooltip.service';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 const routes: Routes = [
   {path: 'networks', component: NetworkListComponent},
@@ -32,7 +33,8 @@ const routes: Routes = [
     NetworkDetailsComponent,
     CreateNetworkComponent,
     UpdateNetworkComponent,
-    DateAgoPipe],
+    DateAgoPipe,
+    NavMenuComponent],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
