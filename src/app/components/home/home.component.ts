b0e079f8e1e69e55eea600de248e9d3b87c1cd7a
Wilmir Nicanor <wbnicanor01@gmail.com>
2020-08-10 23:35:37 +0100

    adds a reroute from home to networks page if user is authenticated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from 'src/app/auth/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService, private router:Router) { }
 
   ngOnInit(): void {
+    if(this.authService.isAuthenticated()){
+      this.router.navigateByUrl("/networks");
+    }
   }
 
 }
